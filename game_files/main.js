844d00e4043f1cd6ec640e38c799a8825c6b4734
gregory <gregory.dossantos98@gmail.com>
2021-06-16 16:33:25 +0200

    add swith world

--- game_files/main.js
+++ game_files/main.js
@@ -11,9 +11,21 @@ window.addEventListener("load", function(event) {
 
   "use strict";
 
-  //// CONSTANTS ////
+  var listScript = document.getElementsByTagName("script");
+  for (let index = 0; index < listScript.length; index++) {
+    const unScript = listScript[index].src;
+    if(unScript.includes('game_files/main.js')) {
+      var world = unScript.split("world=")[1];
+      
+      console.log(world)
+    }
+      
+    
+  }
 
-  const ZONE_PREFIX = "game_files/map/zone";
+  //// CONSTANTS ////
+  
+  const ZONE_PREFIX = "game_files/map/world"+world+"/zone";
   const ZONE_SUFFIX = ".json";
 
       /////////////////
